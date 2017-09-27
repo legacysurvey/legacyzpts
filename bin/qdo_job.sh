54602faf093e18da264ea4c7f22ff737cc50f74c
Kaylan Burleigh <kburleigh@lbl.gov>
2017-09-26 18:13:56 -0700

    updated job scripts for qdo and slurm for eBOSS DR5 run

--- bin/qdo_job.sh
+++ bin/qdo_job.sh
@@ -4,21 +4,18 @@
 # qdo launch obiwan 3 --cores_per_worker 4 --batchqueue debug --walltime 00:05:00 --script $obiwan_code/obiwan/bin/qdo_job_test.sh --keep_env
 
 export camera=decam
-export name_for_run=dr6
+export name_for_run=ebossDR5
 export proj_dir=/project/projectdirs/cosmo/staging
 export scr_dir=/global/cscratch1/sd/kaylanb/zpts_out/${name_for_run}
 export image_fn="$1"
 
 # Load production env and env vars
 source $CSCRATCH/zpts_code/legacyzpts/etc/modulefiles/bashrc_nersc
-
-# Load env, env vars
-source $CSCRATCH/obiwan_code/obiwan/bin/run_atnersc/prodenv_obiwan 
 # check have new env vars
 : ${zpts_code:?}
 
 # Redirect logs
-export log=`echo $image_fn|sed s#${proj_dir}#${scr_dir}#g`
+export log=`echo $image_fn|sed s#${proj_dir}#${scr_dir}#g|sed s#.fits.fz#.log#g`
 mkdir -p $(dirname $log)
 echo Logging to: $log
 
@@ -32,6 +29,7 @@ export threads=1
 cd $zpts_code/legacyzpts/py
 python legacyzpts/legacy_zeropoints.py \
 	--camera ${camera} --image ${image_fn} --outdir ${scr_dir} \
-    >> $log 2>&1d
+  --ps1_only \
+    >> $log 2>&1
 
 
--- bin/slurm_job.sh
+++ bin/slurm_job.sh
@@ -15,7 +15,7 @@ export scr_dir=/global/cscratch1/sd/kaylanb/zpts_out/${name_for_run}
 export image_list=$scr_dir/image_list.txt
 
 usecores=1
-export image_fn=`head ${image_list} -n 1`
+export image_fn=`tail ${image_list} -n 1`
 
 # Load production env and env vars
 source $CSCRATCH/zpts_code/legacyzpts/etc/modulefiles/bashrc_nersc
@@ -23,7 +23,7 @@ source $CSCRATCH/zpts_code/legacyzpts/etc/modulefiles/bashrc_nersc
 : ${zpts_code:?}
 
 # Redirect logs
-export log=`echo $image_fn|sed s#${proj_dir}#${scr_dir}#g`
+export log=`echo $image_fn|sed s#${proj_dir}#${scr_dir}#g|sed s#.fits.fz#.log#g`
 mkdir -p $(dirname $log)
 echo Logging to: $log
 
@@ -38,4 +38,5 @@ cd $zpts_code/legacyzpts/py
 srun -n 1 -c ${usecores} \
 	python legacyzpts/legacy_zeropoints.py \
 	--camera ${camera} --image ${image_fn} --outdir ${scr_dir} \
+  --ps1_only \
     >> $log 2>&1
