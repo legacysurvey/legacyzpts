12ebda78a419856194c8e0b3f02ba6383dcb15ed
Kaylan Burleigh <kburleigh@lbl.gov>
2017-09-28 06:35:35 -0700

    funcs for adding CP header FWHM to zeropoint tables

--- py/legacyzpts/common.py
+++ py/legacyzpts/common.py
@@ -9,6 +9,8 @@ import pandas as pd
 import numpy as np
 import fitsio
 from glob import glob
+from collections import defaultdict
+import json
 
 try:
     from astrometry.util.fits import fits_table, merge_tables
@@ -119,5 +121,85 @@ def shrink_img(camera='decam'):
     fn= fn.replace("oki","ood").replace('ooi','ood')
     fn_new= fn_new.replace("oki","ood").replace('ooi','ood')
     _shrink_img(fn, fn_new, camera=camera)
-    
 
+
+def add_fwhmcp_to_legacypipe_table(T, json_fn='json.txt'):
+  """adds fwhm from CP header as a new colum for each ccd"""
+  proj_fn='/project/projectdirs/cosmo/staging/'
+  fwhm_cp= defaultdict(dict)
+  fns= set(np.char.strip(T.image_filename))
+  for cnt,fn in enumerate(fns):
+    print('%d/%d' % (cnt+1,len(fns)))
+    hdu= fitsio.FITS(proj_fn + fn)
+    isImg= np.char.strip(T.image_filename) == fn
+    for ccdname in np.char.strip(T[isImg].ccdname):
+      h= hdu[ccdname].read_header()
+      fwhm_cp[fn][ccdname]= h['FWHM'] * 0.262 
+  import json
+  json.dump(fwhm_cp, open(json_fn,'w'))
+  print('Wrote %s' % json_fn)
+
+def add_fwhmcp():
+  root= '/global/cscratch1/sd/kaylanb/zpts_out/ebossDR5/'
+  T=fits_table(root+"decam/merged_legacypipe_nocuts.fits.gz")
+  
+  #bad=T.copy()
+  #bad.cut( np.isfinite(bad.fwhm) == False)
+  #add_fwhmcp_to_legacypipe_table(bad, json_fn='fwhmcp_where_nan.json')
+  
+  good=T.copy()
+  good.cut( np.isfinite(good.fwhm))
+  #good= good[:500]
+  add_fwhmcp_to_legacypipe_table(good, json_fn='fwhmcp_where_real.json')
+
+def _insert_fwhmcp(T, json_fn):
+  f= open(json_fn, 'r')
+  fwhm_cp= json.loads(f.read())
+  data= np.zeros(len(T)) -1
+  for fn in fwhm_cp.keys():
+    hasFn= np.char.strip(T.image_filename) == fn
+    for ext in fwhm_cp[fn].keys():
+      hasCCD= np.char.strip(T.ccdname) == ext
+      data[(hasCCD) & (hasFn)]= fwhm_cp[fn][ext]
+  T.set('fwhm_cp',data)
+  return T
+
+def replace_nans():
+  T= fits_table("decam/merged_legacypipe_nocuts.fits.gz") 
+  f= open('fwhmcp_where_nan.json', 'r')
+  fwhm_cp= json.loads(f.read())
+  data= T.fwhm
+  fns= fwhm_cp.keys()
+  for cnt,fn in enumerate(fns):
+    print('%d/%d' % (cnt+1,len(fns)))
+    hasFn= np.char.strip(T.image_filename) == fn
+    for ext in fwhm_cp[fn].keys():
+      hasCCD= np.char.strip(T.ccdname) == ext
+      data[(hasCCD) & (hasFn)]= fwhm_cp[fn][ext] / 0.262
+  T.set('fwhm',data)
+  return T
+
+def replace_all():
+  T= replace_nans()
+  T.writeto('merged_legacypipe_nocuts_replace_nans.fits')
+  f= open('fwhmcp_where_real.json', 'r')
+  fwhm_cp= json.loads(f.read())
+  data= T.fwhm
+  fns= fwhm_cp.keys()
+  for cnt,fn in enumerate(fns):
+    print('%d/%d' % (cnt+1,len(fns)))
+    hasFn= np.char.strip(T.image_filename) == fn
+    for ext in fwhm_cp[fn].keys():
+      hasCCD= np.char.strip(T.ccdname) == ext
+      data[(hasCCD) & (hasFn)]= fwhm_cp[fn][ext] / 0.262
+  T.set('fwhm',data)
+  T.writeto('merged_legacypipe_nocuts_replace_all.fits')
+
+def insert_fwhmcp():
+  # replace Nans with something
+  leg=fits_table("decam/merged_legacypipe_nocuts.fits.gz") 
+  #leg.cut(np.isfinite(leg.fwhm) == False)
+  #return _insert_fwhmcp(leg,'fwhmcp_where_nan.json')  
+  leg.cut(np.isfinite(leg.fwhm))
+  leg=leg[:500]
+  return _insert_fwhmcp(leg,'fwhmcp_where_real.json')  
