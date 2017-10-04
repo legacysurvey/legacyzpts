d51d58140b5216687faef2c08f359ab73b1eb409
Kaylan Burleigh <kburleigh@lbl.gov>
2017-10-03 23:34:57 -0700

    find_images works for all 3 cameras

--- py/legacyzpts/runmanager/find_images.py
+++ py/legacyzpts/runmanager/find_images.py
@@ -9,6 +9,10 @@ from legacyzpts.common import getbash,writelist
 PROJA='/global/projecta/projectdirs/cosmo/staging/'
 PROJ='/project/projectdirs/cosmo/staging/'
 CAMERAS= ['decam','mosaic','90prime']
+AKA= {'decam':'decam/DECam_CP',
+      'mosaic':'mosaicz/MZLS_CP',
+      '90prime':'bok/BOK_CP'}
+      
 
 def isCamera(camera):
   return camera in CAMERAS
@@ -25,14 +29,14 @@ def get_abspath(imgname,camera='decam'):
   assert(isCamera)
   name= imgname.replace(".fz","").replace(".fits","")
   try: 
-    cmd= 'find %s -name %s*' % (PROJ + camera, name)
+    cmd= 'find %s -name %s*' % (PROJ + AKA[camera], name)
     out= getbash(cmd)
     if len(out) == 0:
       raise ValueError
   except ValueError:
     # getbash() failed OR succeeded but found nothing
     try: 
-      cmd= 'find %s -name %s*fits.fz' % (PROJA + camera, name)
+      cmd= 'find %s -name %s*fits.fz' % (PROJA + AKA[camera], name)
       out= getbash(cmd)
     except ValueError:
       raise ValueError('imgname doesnt exist %s' % imgname)
@@ -51,7 +55,7 @@ def get_abspaths(imgname_list,camera='decam',
   #for cnt,imgname in enumerate(['c4d_130220_043932_ooi_z_a1.fits']):
     if cnt % 10 == 0: 
       print("Found %d/%d" % (cnt+1,len(imgname_list)))
-    out= get_abspath(imgname,camera='decam')
+    out= get_abspath(imgname,camera=camera)
     out= out.decode('utf-8') 
     fn_list= out.split('\n')
     if len(fn_list[1]) != 0:
@@ -66,16 +70,21 @@ def get_abspaths(imgname_list,camera='decam',
 
 
 if __name__ == "__main__":
-  dr= '/global/cscratch1/sd/kaylanb/zpts_out/cosmos_dessn'
-  for fn in ['COSMOS_decals-filelist.txt',
-             'DESSNX3_decals-filelist.txt']:
-    outfn= os.path.join(dr,fn).replace('.txt','-abs.txt')
-    if not os.path.exists(outfn):
-      images= np.loadtxt(os.path.join(dr,fn),dtype=str)
-      fns= get_abspaths(images,camera='decam',
-                        ignore_duplicates=True) 
-      writelist(fns, outfn)
-    else:
-      print('Already exists %s' % outfn)
+  from argparse import ArgumentParser
+  parser = ArgumentParser()
+  parser.add_argument('--camera',default=None,help='',required=True)
+  parser.add_argument('--image_list',default=None,help='text file listing c4*.fits.fz like image names',required=True)
+  parser.add_argument('--outdir',default='.',help='',required=False)
+  args = parser.parse_args()
+ 
+  assert('.txt' in args.image_list)
+  outfn= args.image_list.replace('.txt','_abs.txt')
+  if not os.path.exists(outfn):
+    images= np.loadtxt(args.image_list,dtype=str)
+    fns= get_abspaths(images,camera=args.camera,
+                      ignore_duplicates=True) 
+    writelist(fns, outfn)
+  else:
+    print('Already exists %s' % outfn)
 
 
