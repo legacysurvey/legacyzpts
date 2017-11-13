942ac589fd78df7b2417cf86f10b311937a71f65
Kaylan Burleigh <kburleigh@lbl.gov>
2017-11-13 11:35:11 -0800

    added cut on badexp using 'svn export' to get the most recent badexp file for each camera

--- py/legacyzpts/cuts.py
+++ py/legacyzpts/cuts.py
@@ -53,17 +53,22 @@ class LegacyzptsCuts(object):
         assert(camera in CAMERAS)
         self.camera= camera
         self.T= T_zpt.copy()
-        self.good= np.ones(len(self.T),bool)
         self.df= pd.DataFrame({'err':big2small_endian(self.T.err_message),
                                'band':big2small_endian(self.T.filter)})
         self.df['err']= self.df['err'].str.strip()
 
-    def cuts(self):
-        n0 = sum(self.good)
+    def cuts(self,good=None):
+        """Returns bool array, True where CCD should be kept"""
+        if good is None:
+            good= np.ones(len(self.T),bool)
+        else:
+            assert(len(good) == len(self.T))
+
+        n0 = sum(good)
         for name in set(self.df.loc[self.df['err'].str.len() > 0,'err']):
-            self.good[self.df['err'] == name] = False
+            good[self.df['err'] == name] = False
             #continue as usual
-            n = sum(self.good)
+            n = sum(good)
             print('Flagged', n0-n, 'more:',
                   name)
             n0 = n
@@ -71,12 +76,13 @@ class LegacyzptsCuts(object):
             ("not grz filter", (self.correct_bands())),
             #('not 1/3 pix interpolated', (self.third_pix())),
         ]:
-            self.good[crit] = False
+            good[crit] = False
             #continue as usual
-            n = sum(self.good)
+            n = sum(good)
             print('Flagged', n0-n, 'more:',
                   name)
             n0 = n
+        return good
 
     #def err_message(self):
     #    self.good[self.df['err'].str.len() > 0]= False
@@ -97,26 +103,42 @@ class LegacyzptsCuts(object):
                 '90prime':['g','r']
                }[self.camera]
 
+def get_badexp_fn(camera):
+    url='https://desi.lbl.gov/svn/decam/code/'
+    if camera == 'decam':
+        url += 'observing/trunk/obstatus/bad_expid.txt'
+    elif camera == 'mosaic':
+        url += 'mosaic3/trunk/obstatus/bad_expid.txt'
+    dobash('svn export %s %s_bad_expid.txt --force' % (url,camera))
+    return '%s_bad_expid.txt' % camera
+
 
 class LegacypipeCuts(object):
     """Applies legacyipe cuts
 
     Args: 
         T_leg: '*-zpt.fits' table
+        camera:
+        good: bool array to start with, if None then all True
     """
     def __init__(self,T_zpt,camera=None):
         assert(camera in CAMERAS)
         self.camera= camera
         self.ccds= create_legacypipe_table(T=T_zpt.copy(), camera=self.camera)
-        self.good= np.ones(len(self.ccds),bool)
         # From legacypipe
         if self.camera == 'decam':
             self.ccds.ccdzpt += 2.5 * np.log10(self.ccds.exptime)
 
-    def cuts(self):
+    def cuts(self, good=None):
+        """Returns bool array, True where CCD should be kept"""
+        if good is None:
+            good= np.ones(len(self.ccds),bool)
+        else:
+            assert(len(good) == len(self.ccds))
+
         z0 = self.nominal_zeropoints()
         z0 = np.array([z0[f[0]] for f in self.ccds.filter])
-        n0 = sum(self.good)
+        n0 = sum(good)
         for name,crit in [
             ('exptime < 30 s', (self.ccds.exptime < 30)),
             ('ccdnmatch < 20', (self.ccds.ccdnmatch < 20)),
@@ -128,24 +150,26 @@ class LegacypipeCuts(object):
              (self.ccds.zpt > self.max_zeropoint(z0))),
             ('sky too bright',
              (self.sky_too_bright())),
-            #('bad exposure',
-            # (self.bad_exposures())),
+            ('bad exposure',
+             (self.bad_exposure())),
         ]:
-            self.good[crit] = False
+            good[crit] = False
             #continue as usual
-            n = sum(self.good)
+            n = sum(good)
             print('Flagged', n0-n, 'more:',
                   name)
             n0 = n
+        return good
 
-    def bad_exposures(self,bad_expid_fn):
-        if self.camera in ['90prime','mosaic']:
-            bad_expids = np.loadtxt(bad_expid_fn, dtype=int, usecols=(0,))
-            #import legacyccds
-            #fn = os.path.join(os.path.dirname(legacyccds.__file__),
-            #                  'bad_expid_mzls.txt')
-            for expnum in bad_expids:
-                self.good[self.ccds.expnum == expnum]= False
+    def bad_exposure(self):
+        if self.camera == '90prime':
+            print('Skipping, badexp file does not exist for 90prime')
+            return np.zeros(len(self.ccds),bool)
+        else:
+            fn= get_badexp_fn(self.camera)
+            badexp=pd.read_csv(fn,sep='\s+',header=None,usecols=[0],names=['expnum'],comment='#')
+            isBad= pd.Series(big2small_endian(self.ccds.expnum)).isin(badexp['expnum'])
+            return isBad.values 
 
     def sky_too_bright(self):
         if self.camera == 'mosaic':
@@ -209,18 +233,17 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     T_zpt= fits_table(args.zpt_fn)
+    # Empty filter strings mess up "for b in ccd.filter"
     isEmpty= T_zpt.filter == ' '
     if len(T_zpt[isEmpty]) > 0:
-        print('Warning removing %d CCDs because filter is " "' % len(T_zpt[isEmpty]))
+        print('Removed %d, filter is empty string' % len(T_zpt[isEmpty]))
         T_zpt.cut(isEmpty == False)
 
     zpt_cuts= LegacyzptsCuts(T_zpt,args.camera)
-    zpt_cuts.cuts()
+    good= zpt_cuts.cuts()
 
     leg_cuts= LegacypipeCuts(T_zpt,args.camera)
-    leg_cuts.cuts()
+    good= leg_cuts.cuts(good=good)
 
-    good= ((zpt_cuts.good) & 
-           (leg_cuts.good))
     write_survey_ccds(good,T_zpt,camera=args.camera)
 
