b2bd945e3fc59b8570ae9ade60925d7d4a745abc
Dustin Lang <dstndstn@gmail.com>
2017-11-24 00:13:03 -0800

    continue exploring PSF zeropoint cuts

--- py/legacyzpts/compare-psf-ap.py
+++ py/legacyzpts/compare-psf-ap.py
@@ -8,7 +8,7 @@ from astrometry.util.plotutils import PlotSequence
 plt.subplots_adjust(hspace=0.01, wspace=0.01, left=0.15, bottom=0.1, top=0.95, right=0.95)
 
 def run(apfn,psffn,plotfn,tt,band,zplolo,zplo,zphi,pixscale,
-        zpt_cut_lo, zpt_cut_hi):
+        zpt_cut_lo, zpt_cut_hi, bad_expid):
 
     ps = PlotSequence('zp-' + plotfn)
     #A = fits_table('~desiproc/dr6/survey-ccds-mosaic-legacypipe.fits.gz')
@@ -19,16 +19,26 @@ def run(apfn,psffn,plotfn,tt,band,zplolo,zplo,zphi,pixscale,
     print(len(A), 'aperture')
     print(len(P), 'PSF')
 
+    A.ccdzpt = A.zpt
+    A.ccdphrms = A.phrms
+
+    # P.ccdzpt = P.zpt
+    # P.ccdphrms = P.phrms
+    # P.ccdnmatch = P.nmatch_photom
+
     #A.about()
     #P.about()
 
     print('Aperture unique bands:', np.unique(A.filter))
     print('PSF unique bands:', np.unique(P.filter))
-    
-    A.cut([f.strip() == band for f in A.filter])
-    P.cut([f.strip() == band for f in P.filter])
+
+    A.cut(np.array([f.strip() == band for f in A.filter]))
+    P.cut(np.array([f.strip() == band for f in P.filter]))
     print('Cut to', len(A), 'aperture in band', band)
     print('Cut to', len(P), 'PSF in band', band)
+
+    #print('PSF error messages:')
+    #print(np.unique(P.err_message))
     
     amap = dict([((expnum,ccdname.strip()),i) for i,(expnum,ccdname)
                  in enumerate(zip(A.expnum, A.ccdname))])
@@ -63,21 +73,36 @@ def run(apfn,psffn,plotfn,tt,band,zplolo,zplo,zphi,pixscale,
     
     seeing = np.isfinite(P.fwhm) * P.fwhm * pixscale
     P.ccdzpt[np.logical_not(np.isfinite(P.ccdzpt))] = 0.
-    I = np.flatnonzero(#(A.ccd_cuts == 0) *
-                       np.isfinite(A.ccdzpt) *
-                       (P.ccdzpt >= zpt_cut_lo) *
-                       (P.ccdzpt <= zpt_cut_hi) *
-                       (P.ccdphrms < 0.1) *
-                       (seeing < 3.0) * (seeing > 0)
-        )
+    I = np.flatnonzero(
+        #(A.ccd_cuts == 0) *
+        np.isfinite(A.ccdzpt) *
+        (P.ccdzpt >= zpt_cut_lo) *
+        (P.ccdzpt <= zpt_cut_hi) *
+        (P.ccdphrms < 0.1) *
+        (P.ccdrarms  < 0.25) *
+        (P.ccddecrms < 0.25) *
+        (P.exptime > 30) *
+        (np.abs(P.ccdzpt - P.zpt) < 0.25) *
+        (seeing < 3.0) * (seeing > 0)
+    )
     #(seeing < 2.5) * (seeing > 0)
 
     A.cut(I)
     P.cut(I)
     print(len(A), 'pass cuts')
 
+    #I = np.flatnonzero(np.logical_not(np.isfinite(A.ccdzpt)))
+    #print(len(I), 'AP zpts are NaN')
+    #A.ccdzpt[I] = 0.
+
     print('After cuts: AP  zpt range', A.ccdzpt.min(), A.ccdzpt.max())
     print('After cuts: PSF zpt range', P.ccdzpt.min(), P.ccdzpt.max())
+
+    #print('PSF error messages:')
+    #print(np.unique(P.err_message))
+
+    bad = np.array([expnum in bad_expid for expnum in P.expnum])
+    print(sum(bad), 'exposures are in the bad_expid file')
     
     plt.clf()
     mn,mx = zplo,zphi
@@ -86,6 +111,7 @@ def run(apfn,psffn,plotfn,tt,band,zplolo,zplo,zphi,pixscale,
     plt.xlabel('Aperture zeropoint')
     plt.ylabel('PSF zeropoint')
     plt.axis([mn,mx,mn,mx])
+    plt.title(tt)
     ps.savefig()
     
     plt.clf()
@@ -95,6 +121,7 @@ def run(apfn,psffn,plotfn,tt,band,zplolo,zplo,zphi,pixscale,
     plt.xlabel('Aperture zeropoint')
     plt.ylabel('PSF - Aperture zeropoint')
     plt.axis([mn,mx, -1,1])
+    plt.title(tt)
     ps.savefig()
     
     # plt.clf()
@@ -129,6 +156,42 @@ def run(apfn,psffn,plotfn,tt,band,zplolo,zplo,zphi,pixscale,
              histtype='step', log=True)
     plt.xlabel('PSF - Aperture ccdzpt')
     plt.xlim(mn, mx)
+    plt.suptitle(tt)
+    ps.savefig()
+
+
+    plt.clf()
+    mn,mx = 0,0.25
+    plt.hist(np.clip(P.ccdrarms ,mn,mx), bins=50, range=(mn,mx), histtype='step', color='b', label='RA' , log=True)
+    plt.hist(np.clip(P.ccddecrms,mn,mx), bins=50, range=(mn,mx), histtype='step', color='g', label='Dec', log=True)
+    plt.xlim(mn,mx)
+    plt.legend()
+    plt.xlabel('Astrometric scatter')
+    plt.suptitle(tt)
+    ps.savefig()
+
+    plt.clf()
+    plt.hist(P.ccdskycounts,  bins=50, log=True)
+    plt.xlabel('CCD sky counts')
+    plt.suptitle(tt)
+    ps.savefig()
+
+    plt.clf()
+    plt.hist(P.exptime,  bins=50, log=True)
+    plt.xlabel('Exptime (s)')
+    plt.suptitle(tt)
+    ps.savefig()
+
+    plt.clf()
+    plt.hist(P.ccdzpt - P.zpt,  bins=50, log=True)
+    plt.xlabel('CCD zpt - average zpt')
+    plt.suptitle(tt)
+    ps.savefig()
+
+    plt.clf()
+    plt.hist(P.fwhm * pixscale,  bins=50, log=True)
+    plt.xlabel('Seeing (arcsec)')
+    plt.suptitle(tt)
     ps.savefig()
     
     J = np.flatnonzero(np.logical_or((P.ccdzpt - A.ccdzpt) < -0.1,
@@ -136,49 +199,90 @@ def run(apfn,psffn,plotfn,tt,band,zplolo,zplo,zphi,pixscale,
     print('Some bad ones:')
     for j in J:
         print(P.image_filename[j].strip(), 'PSF nmatch', P.ccdnmatch[j], 'phrms', P.ccdphrms[j], 'AP phrms', A.ccdphrms[j], 'exptime', A.exptime[j], 'seeing', A.fwhm[j] * pixscale)
+        print('  AP err', A.err_message[j], 'CCD cuts', A.ccd_cuts[j])
+        print('  Expnum', P.expnum[j], 'Bad expid:', bad_expid.get(int(P.expnum[j]), '(none)'))
 
-        # plt.clf()
-        # #for i in range(4):
-        # #plt.subplot(2,2,i+1)
-        # img = fitsio.read(P.image_filename[j].strip(), ext=P.ccdname[j].strip()) #ext=i+1)
-        # mn,mx = np.percentile(img.ravel(), [25,98])
-        # plt.imshow(img, interpolation='nearest', origin='lower', vmin=mn, vmax=mx)
-        # plt.title('%s %s' % (P.image_filename[j].strip(), P.ccdname[j].strip()))
-        # ps.savefig()
-        # 
-        # plt.clf()
-        # plt.subplot(1,2,1)
-        # fn = P.image_filename[j].strip().replace('_ooi_','_ood_')
-        # dq = fitsio.read(fn, ext=P.ccdname[j].strip())
-        # mn,mx = 0, 1
-        # plt.imshow(dq, interpolation='nearest', origin='lower', vmin=mn, vmax=mx)
-        # plt.xticks([]); plt.yticks([])
-        # plt.title('DQ')
-        # 
-        # plt.subplot(1,2,2)
-        # fn = P.image_filename[j].strip().replace('_ooi_','_oow_')
-        # wt = fitsio.read(fn, ext=P.ccdname[j].strip())
-        # mn,mx = 0, wt.max()
-        # plt.imshow(wt, interpolation='nearest', origin='lower', vmin=mn, vmax=mx)
-        # plt.xticks([]); plt.yticks([])
-        # plt.title('WT')
-        # plt.suptitle('%s %s' % (P.image_filename[j].strip(), P.ccdname[j].strip()))
-        # ps.savefig()
+        ttxt = '%s %s %i' % (P.image_filename[j].strip().replace('.fits.fz','').replace('_ooi','').replace('_zd',''),
+                          P.ccdname[j].strip(), P.expnum[j])
+
+        plt.clf()
+        #for i in range(4):
+        #plt.subplot(2,2,i+1)
+        img = fitsio.read(P.image_filename[j].strip(), ext=P.ccdname[j].strip()) #ext=i+1)
+        mn,mx = np.percentile(img.ravel(), [25,98])
+        plt.imshow(img, interpolation='nearest', origin='lower', vmin=mn, vmax=mx)
+        plt.title(ttxt)
+        ps.savefig()
+
+        H,W = img.shape
+        #plt.clf()
+        #plt.imshow(img[H//2-250:H//2+250, W//2-250:W//2+250],
+        #               interpolation='nearest', origin='lower', vmin=mn, vmax=mx)
+        plt.axis([W//2-250, W//2+250, H//2-250, H//2+250])
+        plt.title(ttxt)
+        ps.savefig()
+
+        
+        plt.clf()
+        plt.subplot(1,2,1)
+        fn = P.image_filename[j].strip().replace('_ooi_','_ood_')
+        dq = fitsio.read(fn, ext=P.ccdname[j].strip())
+        mn,mx = 0, 1
+        plt.imshow(dq, interpolation='nearest', origin='lower', vmin=mn, vmax=mx)
+        plt.xticks([]); plt.yticks([])
+        plt.title('DQ')
+        
+        plt.subplot(1,2,2)
+        fn = P.image_filename[j].strip().replace('_ooi_','_oow_')
+        wt = fitsio.read(fn, ext=P.ccdname[j].strip())
+        mn,mx = 0, wt.max()
+        plt.imshow(wt, interpolation='nearest', origin='lower', vmin=mn, vmax=mx)
+        plt.xticks([]); plt.yticks([])
+        plt.title('WT')
+        plt.suptitle(ttxt)
+        ps.savefig()
 
 
 if __name__ == '__main__':
+
+    bad_expid = {}
+    f = open('bad_expid.txt')
+    for line in f.readlines():
+        #print(line)
+        if len(line) == 0:
+            continue
+        if line[0] == '#':
+            continue
+        words = line.split()
+        if len(words) < 2:
+            continue
+        try:
+            expnum = int(words[0], 10)
+        except:
+            print('Skipping line', line)
+            continue
+        reason = ' '.join(words[1:])
+        bad_expid[expnum] = reason
+
     for X in [
-        ('apzpts/survey-ccds-90prime-legacypipe.fits.gz',
-         'survey-ccds-90prime-psfzpts.fits',
-         'g', 'BASS g', 'g', 20, 25, 26.25, 0.45,
-         25.2, 26.0),
-        ('apzpts/survey-ccds-90prime-legacypipe.fits.gz',
-         'survey-ccds-90prime-psfzpts.fits',
-         'r', 'BASS r', 'r', 19.5, 24.75, 25.75, 0.45,
-         24.9, 25.7),
-        ('apzpts/survey-ccds-mosaic-legacypipe.fits.gz',
-         'survey-ccds-mosaic-psfzpts.fits',
-         'z', 'MzLS z', 'z', 19.5, 25, 27, 0.262,
-         25.2, 26.8),
-         ]:
+            (#'apzpts/survey-ccds-90prime-legacypipe.fits.gz',
+                'apzpts/survey-ccds-90prime.fits.gz',
+                'survey-ccds-90prime-psfzpts.fits',
+                #'90prime-psfzpts.fits',
+                'g', 'BASS g', 'g', 20, 25, 26.25, 0.45,
+                25.2, 26.0, {}),
+            (#'apzpts/survey-ccds-90prime-legacypipe.fits.gz',
+                'apzpts/survey-ccds-90prime.fits.gz',
+                'survey-ccds-90prime-psfzpts.fits',
+                #'90prime-psfzpts.fits',
+                'r', 'BASS r', 'r', 19.5, 24.75, 25.75, 0.45,
+                24.9, 25.7, {}),
+            (#'apzpts/survey-ccds-mosaic-legacypipe.fits.gz',
+                'apzpts/survey-ccds-mosaic.fits.gz',
+                'survey-ccds-mosaic-psfzpts.fits',
+                #'mosaic-psfzpts.fits',
+                'z', 'MzLS z', 'z', 19.5, 25, 27, 0.262,
+                25.2, 26.8, bad_expid),
+    ]:
         run(*X)    
+
