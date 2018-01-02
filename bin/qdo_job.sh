d4f458b1484a9facc288c2b1c48fba742c44b3ab
Kaylan Burleigh <kburleigh@lbl.gov>
2018-01-02 09:52:50 -0800

    updated slurm scripts

--- bin/qdo_job.sh
+++ bin/qdo_job.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
 # Example
-# qdo launch obiwan 3 --cores_per_worker 4 --batchqueue debug --walltime 00:05:00 --script $obiwan_code/obiwan/bin/qdo_job_test.sh --keep_env
+# qdo launch dr6_zpts 32 --cores_per_worker 1 --batchqueue debug --walltime 00:30:00 --script $obiwan_code/obiwan/bin/qdo_job_test.sh --keep_env
 
-export camera=decam
-export name_for_run=cosmos_dessn
+export name_for_run=dr6
 export proj_dir=/project/projectdirs/cosmo/staging
 export proja_dir=/global/projecta/projectdirs/cosmo/staging
 export scr_dir=/global/cscratch1/sd/kaylanb/zpts_out/${name_for_run}
-export image_fn="$1"
+export camera="$1"
+export image_fn="$2"
 
 # Load production env and env vars
 source $CSCRATCH/zpts_code/legacyzpts/etc/modulefiles/bashrc_nersc
@@ -26,6 +26,23 @@ export MPICH_GNI_FORK_MODE=FULLCOPY
 export MKL_NUM_THREADS=1
 export OMP_NUM_THREADS=1
 export threads=1
+
+# Limit memory to avoid 1 srun killing whole node
+if [ "$NERSC_HOST" = "edison" ]; then
+    # 62 GB / Edison node = 65000000 kbytes
+    maxmem=65000000
+    let usemem=${maxmem}*${threads}/24
+else
+    # 128 GB / Cori node = 65000000 kbytes
+    maxmem=134000000
+    let usemem=${maxmem}*${threads}/32
+fi
+echo BEFORE
+ulimit -Sa
+ulimit -Sv $usemem
+echo AFTER
+ulimit -Sa
+
 
 cd $zpts_code/legacyzpts/py
 python legacyzpts/legacy_zeropoints.py \
